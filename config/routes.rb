302e3714555c827c8c3e45e28779c9fe209b8275
Philip Alexander <pjalexander2@gmail.com>
2015-02-11 15:34:34 +0700

    Added user authentication...user not tied to cards yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  resources :cards
+  get "logout" => "sessions#destroy", :as => "log_out"
+  get "login" => "sessions#new", :as => "log_in"
+  get "signup" => "users#new", :as => "sign_up"
   
+  # root :to => "users#new"
+  # note pa: commented out for railscast tutorial for Auth
+  # get 'welcome/index'
+  root 'welcome#index'
+  
+  resources :users
+  resources :cards
+  resources :sessions
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  # root 'welcome#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205020232) do
+ActiveRecord::Schema.define(version: 20150211030520) do
 
   create_table "cards", force: true do |t|
     t.string   "front"
     t.text     "back"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
   end
 
 end
